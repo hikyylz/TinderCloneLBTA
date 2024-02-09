23aa944f802cf6dfe341d8764eedfeaef120a468
Kaan Yıldız <kaanyildiz@Kaan-MacBook-Air-3.local>
2024-02-09 21:37:42 +0300

    Update HomeButtonControlStackView.swift

--- Views/HomeButtonControlStackView.swift
+++ Views/HomeButtonControlStackView.swift
@@ -36,21 +36,6 @@ class HomeButtonControlStackView: UIStackView {
         
         
         
-        /*
-         // arrengedSubview nasıl koyulur görmek için yorum satırı olarak tutacağım burayı.
-         
-        let subViews = [ UIImage(imageLiteralResourceName: "refresh_circle"), UIImage(imageLiteralResourceName: "dismiss_circle"), UIImage(imageLiteralResourceName: "super_like_circle"), UIImage(imageLiteralResourceName: "like_circle"), UIImage(imageLiteralResourceName: "boost_circle")].map { (image) -> UIView in
-            let button = UIButton(type: .system)
-            
-            // .withRe.. methodu image ların ekranda düzgün gözkmesine yardımcı oldu.
-            button.setImage(image.withRenderingMode(.alwaysOriginal) , for: .normal)
-            return button
-        }
-        
-        subViews.forEach { v in
-            addArrangedSubview(v)
-        }
-        */
     }
     
     required init(coder: NSCoder) {
